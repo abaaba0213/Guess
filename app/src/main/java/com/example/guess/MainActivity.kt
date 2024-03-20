73305991b3e2af53b171e72541632425c00992ee
abaaba0213 <mickeyhair@gmail.com>
2024-03-20 17:58:35 +0800

    第三次commit

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun Guess(v: View) {
+        var result: TextView = findViewById(R.id.txvResult)
+
+        var User: Int = 0  //玩家出拳
+        var Phone: Int = (0..2).random()  //手機出拳
+        if(v.id == R.id.imageS){
+            User = 0
+        }else if(v.id == R.id.imageR){
+            User = 1
+        }else if(v.id == R.id.imageP){
+            User = 2
+        }
+        result.text = "玩家出"+ GuessType(User)
+    }
+
+    fun GuessType(x:Int):String{
+        return  when(x){
+            0 -> "剪刀"
+            1 -> "石頭"
+            else -> "布"
+        }
     }
 }
